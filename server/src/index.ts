86b1cebe47c0640d7013301f6ca42d1c59a4d608
paologum <pgumasin@purdue.edu>
2025-02-23 18:38:02 -0500

    add the firebase connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import dotenv from "dotenv";
 import fastifyStatic from "@fastify/static";
+import { db } from "./database/database.js";
 
 dotenv.config();
 const fastify = Fastify({ logger: true });
@@ -12,6 +13,16 @@ fastify.register(fastifyStatic, {
   root: process.cwd() + "/public",
   prefix: "/public/",
   decorateReply: false,
+});
+fastify.get("/users", async (request, reply) => {
+  try {
+    const snapshot = await db.collection("users").get();
+    const users = snapshot.docs.map((doc) => doc.data());
+    return users;
+  } catch (e) {
+    console.error(e);
+  }
+  return [];
 });
 const start = async () => {
   try {
@@ -30,4 +41,4 @@ const start = async () => {
   }
 };
 
-start();
+await start();
